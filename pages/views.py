a314338ba0e6b83155d169643d0047527b70c91e
Jayden Xun <jaydenxun0326@gmail.com>
2026-06-14 22:08:50 -0400

    login screen

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def home(request):
     return render(request, "pages/home.html", {})
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "pages/login.html", {"form": form})
